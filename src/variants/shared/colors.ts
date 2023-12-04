2a3bcbfa774a7bd27622cf025f3d63ff53b941a0
lovrozagar <lovro.zagar5@gmail.com>
2023-12-04 11:28:47 -0800

    style: add spinner variants

---
+++ src/variants/shared/colors.ts
@@ -0,0 +1,17 @@
+const sharedColorVariants = {
+  'mode': [],
+  'mode-contrast': [],
+  'mode-accent': [],
+  'mode-contrast-accent': [],
+  'neutral': [],
+  'primary': [],
+  'secondary': [],
+  'destructive': [],
+  'success': [],
+  'warning': [],
+  'info': [],
+  'help': [],
+  'brand': [],
+} as const
+
+export { sharedColorVariants }
